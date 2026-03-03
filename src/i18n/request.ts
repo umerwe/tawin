d15a2e341614bcf879285a367f67e625ae560ec4
umerwe <uemyy1@gmail.com>
2026-03-03 10:32:00 +0500

    fixed translation issue

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,16 @@
-import {getRequestConfig} from 'next-intl/server';
- 
-export default getRequestConfig(async ({locale}) => {
+import { getRequestConfig } from 'next-intl/server';
+import { routing } from "./routing"
+export default getRequestConfig(async ({ requestLocale }) => {
+
+  let locale = await requestLocale
+
+  // Ensure that a valid locale is used
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale
+  }
+
+  console.log({ localeHere: locale })
+  console.log({ messagePath: `../messages/${locale || 'en'}.json` })
   return {
     locale: locale || 'en',
     messages: (await import(`../messages/${locale || 'en'}.json`)).default
