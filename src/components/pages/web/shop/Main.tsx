f386d5b35c2eaa81f6e8eb0f1758a02ca83a5d22
umerwe <uemyy1@gmail.com>
2026-03-05 23:19:07 +0500

    improved responsiveness

--- src/components/pages/web/shop/Main.tsx
+++ src/components/pages/web/shop/Main.tsx
@@ -20,7 +20,7 @@ const Shop = () => {
     const [viewMode, setViewMode] = useState("grid4");
 
     return (
-        <div className="space-y-8 pb-16 max-w-7xl mx-auto">
+        <div className="space-y-8 pb-16 max-w-7xl mx-auto px-4">
             <Hero activeCategory={category} />
 
             <FilterBar viewMode={viewMode} onViewModeChange={setViewMode} />
