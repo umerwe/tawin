4bde0bea4a7d7296640ac4906b559e9710f3f930
umerwe <uemyy1@gmail.com>
2026-03-13 21:44:13 +0500

    added metadata type

--- src/app/[locale]/admin/layout.tsx
+++ src/app/[locale]/admin/layout.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/common/Container";
 import Navbar from "@/components/layout/admin/Navbar";
 import Sidebar from "@/components/layout/admin/Sidebar";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Dashboard"
